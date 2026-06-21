a6fb6d4e1d1dd644f72c035fa97b44907175d512
angi permana <angi.permana@gmail.com>
2026-06-22 05:46:07 +0700

    Fix keystatic github login 500 error by adding output: hybrid, generating .env in workflow and loading it in server.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,7 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://manasikhajiumrah.net',
+  output: 'hybrid',
   integrations: [mdx(), sitemap(), keystatic(), react(), markdoc()],
   redirects: {
     '/admin': '/keystatic',
--- server.js
+++ server.js
@@ -1,1 +1,3 @@
+import 'dotenv/config';
+
 import('./dist/server/entry.mjs');
